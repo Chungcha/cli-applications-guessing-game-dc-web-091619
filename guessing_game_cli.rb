8b224e6f5463c781283e1a82947e478b39d48b82
Charlie Chung <charlieschung@gmail.com>
2019-09-14 18:35:20 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -15,8 +15,8 @@ def compare(num,input)
     puts "You guessed the correct number!"
   elsif input == "exit"
     puts "Goodbye!"
- # elsif num != input && num != "exit"
-#    puts "Sorry! The computer guessed #{num}."
+  elsif num != input && num != "exit"
+    puts "Sorry! The computer guessed #{num}."
   end
 end
 
