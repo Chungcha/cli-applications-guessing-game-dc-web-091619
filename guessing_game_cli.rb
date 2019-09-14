221262648798e006f45ae13fc799e0b662bbf118
Charlie Chung <charlieschung@gmail.com>
2019-09-14 18:23:21 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,19 +1,17 @@
+def random
+  num=rand(6)+1
+end
 
+def prompt
+  puts "Guess a number!"
+end
 
-
-
-
-
-
-
-
-
-
-
-
+def prompt_input
+  gets.chomp
+end
 
 
 
 def run_guessing_game
-
+  random
 end
